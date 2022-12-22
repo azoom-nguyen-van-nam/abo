f9dca65bf346f0bf6d4e047d64fb5d327e288174
Nguyen Van Nam <nguyen.van.nam@azoom.jp>
2022-12-22 18:22:51 +0000

    :+1: Set up default layout for project

---
+++ components/layout/the-sidebar.vue
@@ -0,0 +1,38 @@
+<script setup>
+const menus = ref([{ label: 'User', to: '/user' }])
+</script>
+
+<template>
+  <nav class="the-sidebar">
+    <nuxt-link to="/">
+      <img class="the-logo" src="/logo.jpeg" alt="abo-logo" width="50" />
+    </nuxt-link>
+    <nuxt-link
+      class="route"
+      v-for="menu in menus"
+      :key="new Date().getTime()"
+      :to="menu.to"
+    >
+      {{ menu.label }}
+    </nuxt-link>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.the-sidebar {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #ccc;
+  > .route {
+    width: 100%;
+    text-align: center;
+    text-decoration: none;
+    padding: 10px;
+    &:hover {
+      color: #2c2cc8;
+      background-color: #f3e2e2;
+    }
+  }
+}
+</style>
