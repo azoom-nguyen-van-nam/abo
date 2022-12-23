a51d03a5b1145f9bc49c258112d869c1e90c2f70
Nguyen Van Nam <nguyen.van.nam@azoom.jp>
2022-12-23 08:36:33 +0000

    :+1: Using middleware to guard pages

---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+useRefHead({
+  title: 'Login',
+  meta: [
+    {
+      name: 'description',
+      content: 'Login'
+    }
+  ]
+})
+definePageMeta({
+  middleware: 'redirect'
+})
+</script>
+<template>
+  <div>Login page</div>
+</template>
