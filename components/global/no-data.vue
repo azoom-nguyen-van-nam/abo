abebd9f2c3c435c4edf1292992fd3e1772ec1993
Pham Thanh Thuy <pham.thanh.thuy@azoom.jp>
2022-12-30 09:06:39 +0000

    :+1: Implement search user, create no-data component

---
+++ components/global/no-data.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="h-100 d-flex justify-center align-center">
+    <img src="~/assets/images/no-data.png" alt="No data" />
+  </div>
+</template>
+
+<style scoped></style>
